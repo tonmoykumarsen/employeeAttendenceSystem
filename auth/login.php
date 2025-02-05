c75d93c4de50bd300d3ffd8cef155cb465120b6a
tonmoykumarsen <jessijesica217@gmail.com>
2025-02-05 16:49:21 +0600

    Login Page Design

---
+++ auth/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login -Page</title>
+    <link rel="stylesheet" href="../asset/CSS/style.css">
+</head>
+<body>
+<!-- Login -->
+ <div class="login-container">
+ <form class="form-login">
+ <label class="label" for="email">Email</label>
+ <input type="text" name="email" id="" placeholder="Enter Your E-mail" >
+<label class="label" for="Password">Password</label>
+<input type="password" name="password" id="" placeholder="Enter Your Password" >
+
+<input class="button" type="submit" value="Login" ></input>
+ </form>
+ </div>
+</body>
+</html>
